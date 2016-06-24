e061dbd3736f1f0847e0fc56d7193dd32aa2a62a
David Andrade <David Andrade>
2016-06-24 00:18:22 -0300

    git commit -m "first commit" git remote add origin https://github.com/davidsmandrade/project00.git git push -u origin master

---
+++ app/src/main/java/localhost/project00/MainActivity.java
@@ -0,0 +1,23 @@
+package localhost.project00;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonCapStone) void onbuttonCapStone() {
+        Toast.makeText(this, "This button will launch my capstone app", Toast.LENGTH_SHORT)
+                .show();
+    }
+}
